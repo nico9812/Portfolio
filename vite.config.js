ddced1bea7d30b4406c42a3f135ff288bae50280
nico9812 <nicocalderon9818@gmail.com>
2025-09-15 18:31:59 -0300

    add

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/mi-portfolio/',
+  base: '/Portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // alias para src
